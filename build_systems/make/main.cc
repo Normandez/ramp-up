d4bcc88e60cc5feda5ff44a8423b324ae6179c29
AlexanderSergienko <sergienko.9711@gmail.com>
2021-03-25 01:44:55 +0200

    build_systems: add make

---
+++ build_systems/make/main.cc
@@ -0,0 +1,8 @@
+#include "utils.h"
+
+int main()
+{
+    print("I'm alive!");
+    return 0;
+}
+
---
+++ build_systems/make/utils.cc
@@ -0,0 +1,9 @@
+#include "utils.h"
+
+#include <iostream>
+
+void print(const char* s)
+{
+    std::cout << s << std::endl;
+}
+
